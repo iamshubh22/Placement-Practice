48565e3681730e1d96cef0777a476ab496f3ab0b
iamshubh22 <shubhdwivedimi@gmail.com>
2022-06-23 19:57:02 +0530

    done

--- Questions/Sliding_window.java
+++ Questions/Sliding_window.java
@@ -1,3 +1,5 @@
+//Window Sliding Technique is a computational technique which aims to reduce the use of nested loop and replace it with a single loop, thereby reducing the time complexity.
+
 package Questions;
 
 public class Sliding_window {
