39207411788bcaaf61dff53ef6ef6856e77892a8
iamshubh22 <shubhdwivedimi@gmail.com>
2022-07-04 14:29:26 +0530

    done

--- Stack/main_sl.java
+++ Stack/main_sl.java
@@ -1,11 +1,14 @@
 public class main_sl {
-    public static void main(String[] args) {
-		stack_l sll=new stack_l();
-		sll.push(0);
-		sll.push(1);
-		sll.push(2);
-		sll.pop();
-		sll.peek();
-		sll.display();
+    public static void main(String[] args){
+		stack_l a=new stack_l();
+		a.push(1);
+		a.push(2);
+		a.push(3);
+		a.push(4);
+		a.push(5);
+		a.display();
+		a.pop();
+		a.display();
+		a.peek();	
 }
 }
--- Stack/stack_l.java
+++ Stack/stack_l.java
@@ -1,62 +1,57 @@
 public class stack_l {
     private class Node
-    {
-    int data;
-    Node link;
-    }
-    Node top;
-    
-    stack_l()
-    {
-    this.top=null;
-    }
-    
-    public void push(int x)
-    {
-    Node temp=new Node();
-    temp.data=x;
-    temp.link=top;
-    top=temp;
-    }
-    public boolean isEmpty()
-    {
-        return top==null;
-    }
-    public int peek()
-    {
-        if(!isEmpty())
-        {
-            return top.data;
-        }
-        else
-        {
-            System.out.println("Stack is Empty");
-            return -1;
-        }
-    }
-    public void pop()
-    {
-        if(top==null)
-        {
-            System.out.println("Stack is empty");
-            return ;
-        }
-        top=(top).link;
-    }
-    public void display()
-    {
-        if (top == null) {
-            System.out.printf("\nStack Underflow");
-           
-        }
-        else {
-            Node temp = top;
-            while (temp != null) {
-    
-                System.out.printf("%d->", temp.data);
-    
-                temp = temp.link;
-            }
-        }
-    }
-    }
+	{
+		int data;
+		Node next;
+	}
+	Node top =null;
+	
+	public void push(int value)
+	{
+		Node temp =new Node();
+		temp.data=value;
+		temp.next=top;
+		top=temp;
+	}
+	
+	public void pop()
+	{
+		if(top==null)
+		{
+			System.out.println("stack empty");
+		}
+		else
+		{
+			System.out.println(top.data);
+			top=top.next;
+		}
+							
+	}
+	public void peek()
+	{
+		if(top==null)
+		{
+			System.out.println("stack empty");
+		}
+		else
+		{
+		System.out.println(top.data);
+		}
+		}
+	public void display()
+	{
+		if(top==null)
+		{
+			System.out.println("Stack is empty");
+		}
+		else
+		{
+			Node temp = top;
+			while(temp!=null)
+			{
+				System.out.println(temp.data);
+				temp=temp.next;
+			}
+		}
+	}
+}
